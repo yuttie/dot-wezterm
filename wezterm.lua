add1c10b7ca452bf6b02a23e5c3eea355a8b560f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-05-29 17:17:09 +0900

    Update config related to text rendering

--- wezterm.lua
+++ wezterm.lua
@@ -20,13 +20,13 @@ end
 return {
   term = 'wezterm',
   cursor_blink_rate = 500,
-  font_size = 7.0,
+  font_size = 8.0,
   font = my_mono_fonts({ weight='DemiBold', style='Normal' }),
   font_rules = {
     {
       italic = true,
       intensity = 'Bold',
-      font = my_mono_fonts({ weight='Bold', style='Oblique' }),
+      font = my_mono_fonts({ weight='Black', style='Oblique' }),
     },
     {
       italic = true,
@@ -34,10 +34,14 @@ return {
     },
     {
       intensity = 'Bold',
-      font = my_mono_fonts({ weight='Bold', style='Normal' }),
+      font = my_mono_fonts({ weight='Black', style='Normal' }),
     },
   },
+  line_height = 0.9,
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+  freetype_interpreter_version = 40,
+  freetype_load_target = "Light",
+  freetype_render_target = "HorizontalLcd",
   keys = {
     { key='c', mods='SHIFT|ALT', action=wezterm.action{CopyTo='Clipboard'} },
     { key='V', mods='SHIFT|ALT', action=wezterm.action{PasteFrom='Clipboard' }},
